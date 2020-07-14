20fb4bca2d70213c3dd032aee13aa9b66162c745
vagrant <vagrant@localhost.localdomain>
2020-07-14 07:23:40 +0000

    [Update]5-9

--- db/migrate/20200708055740_devise_create_users.rb
+++ db/migrate/20200708055740_devise_create_users.rb
@@ -35,7 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name =>  your_custom_name
+      t.string :name 
       t.timestamps null: false
     end
 
